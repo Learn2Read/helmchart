ead5737e0ff069ba6083fd56a05996adb19eed2f
Yu Yu <yu.yu01.sap.com>
2020-09-09 00:07:08 +0800

    add postgres

--- indexHelmchart.sh
+++ indexHelmchart.sh
@@ -1,4 +1,5 @@
 tar -zcf api-server-1.0.0.tgz -C api-server ./
 tar -zcf frontend-1.0.0.tgz -C frontend ./
 tar -zcf keycloak-1.0.0.tgz -C keycloak ./
+tar -zcf postgres-1.0.0.tgz -C postgres ./
 helm repo index ./
