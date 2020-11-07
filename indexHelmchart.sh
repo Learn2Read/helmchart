8d1b519ac22cb602b5f54083f9acf0e2359666e1
Yu Yu <yu.yu01.sap.com>
2020-11-08 00:44:05 +0800

    add es and kibana helmchart

--- indexHelmchart.sh
+++ indexHelmchart.sh
@@ -2,4 +2,5 @@ tar -zcf api-server-1.0.0.tgz -C api-server ./
 tar -zcf frontend-1.0.0.tgz -C frontend ./
 tar -zcf keycloak-1.0.0.tgz -C keycloak ./
 tar -zcf postgres-1.0.0.tgz -C postgres ./
+tar -zcf elasticsearch-1.0.0.tgz -C elasticsearch ./
 helm repo index ./
